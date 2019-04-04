8a5a8cadd71fc6e2957cc506219d3d503c188302
Alexy Samyn <alexy59430@gmail.com>
2019-04-04 17:32:09 +0200

    ajust reponsivity and color of navbar

--- moov-in-lille/src/component/Navbar.jsx
+++ moov-in-lille/src/component/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../style/navbar.css';
 import {
     Collapse,
     Navbar,
@@ -7,11 +8,11 @@ import {
     Nav,
     NavItem} from 'reactstrap';
 import { Button } from 'reactstrap';
+import logo from '../img/logo.png';
 
 class NavbarFull extends Component {
     constructor(props) {
         super(props);
-    
         this.toggle = this.toggle.bind(this);
         this.state = {
           isOpen: false
@@ -20,26 +21,26 @@ class NavbarFull extends Component {
       toggle() {
         this.setState({
           isOpen: !this.state.isOpen
-        });
+          });
       }
       render() {
         return (
-          <div>
-            <Navbar color="light" light expand="xs">
-              <NavbarBrand href="/">Moov'in Lille</NavbarBrand>
-              <NavbarToggler onClick={this.toggle} />
-              <Collapse isOpen={this.state.isOpen} navbar>
-                <Nav className="ml-auto" navbar>
-                  <NavItem>
-                    <Button outline color="secondary"><i className="fas fa-map-marked-alt"></i></Button>{' '}
-                  </NavItem>
-                  <NavItem>
-                    <Button outline color="secondary"><i className="far fa-heart"></i></Button>{' '}
-                  </NavItem>
-                </Nav>
-              </Collapse>
-            </Navbar>
-          </div>
+            <div>
+                <Navbar className="navbarFull" color="light" light expand="xs">
+                    <NavbarBrand href="/"><img src={logo} className="logo" alt="logo moov'in lille"/></NavbarBrand>
+                    <NavbarToggler on onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem className="boutton1">
+                                <Button outline color="danger"><i className="fas fa-map-marked-alt"><br />Itinerary</i></Button>
+                            </NavItem>
+                            <NavItem className="boutton2">
+                                <Button outline color="danger"><i className="fas fa-heart"><br />Favorites</i></Button>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>   
         );
       }
 }
