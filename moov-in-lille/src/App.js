188a5e9ef52b014029f8a40ccf6f202d068a7374
Sabine Robart <sabine.robart@protonmail.com>
2019-04-09 22:49:48 +0200

    Modifies component import after changing MyMap component filename.

--- moov-in-lille/src/App.js
+++ moov-in-lille/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
-import MyMap from './component/Map';
+import React, { Component } from "react";
+import "./App.css";
+import MyMap from "./component/MyMap";
 
 class App extends Component {
   render() {
