23403d333bf824cb244be984f32c8c755854ee64
Sabine Robart <sabine.robart@protonmail.com>
2019-04-12 10:07:47 +0200

    Axios : get V'lille API.

---
+++ moov-in-lille/src/component/DataContainer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import axios from "axios";
+
+class DataContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stations: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&rows=223"
+      )
+      .then(res => {
+        this.setState({
+          stations: res.data
+        });
+      });
+  }
+}
