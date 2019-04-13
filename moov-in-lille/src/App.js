d99c961d6cb886dfa89c609936ddcaa9fbd72f09
Sabine Robart <sabine.robart@protonmail.com>
2019-04-13 11:36:41 +0200

    Changes stylesheet import from 'css' to 'scss'.

--- moov-in-lille/src/App.js
+++ moov-in-lille/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./App.scss";
 import NavbarFull from "./component/NavbarFull";
 import DataContainer from "./component/DataContainer";
 
