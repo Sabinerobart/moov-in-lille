f2083be4cc68e0e33b3c9ff8d9b2340259db58be
LoicBrassart <loic.brassart@gmail.com>
2019-04-16 18:37:54 +0200

    Fixing all (minor) errors for my wonderful students. <3

--- moov-in-lille/src/App.js
+++ moov-in-lille/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.scss";
 import NavbarFull from "./component/NavbarFull";
 import DataContainer from "./component/DataContainer";
-import Footer from './component/footer';
+import Footer from './component/Footer';
 
 class App extends Component {
   render() {
