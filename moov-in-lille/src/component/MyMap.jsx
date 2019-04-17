428d7983e41c0c951fc7ca71a2ffc28260ccc838
FredericMentel <frederic.mentel@gmail.com>
2019-04-17 22:26:36 +0200

    Not necessary to create a proto GridLayer, we have just to use TileLayer in order to use mapbox layer.

--- moov-in-lille/src/component/MyMap.jsx
+++ moov-in-lille/src/component/MyMap.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
-import MapboxLayer from "./MapboxLayer";
 import RangeSlider from "./RangeSlider";
 
-const MAPBOX_ACCESS_TOKEN =
-  "pk.eyJ1IjoiZnJlZDc4OTYiLCJhIjoiY2p1YmJ2dnM5MDRkYTN6cW1nZHJoc3pudiJ9.UdKtx13HOR9-Uoej4C5cyw";
+//OSM Default MapTiles - Keep for feature - custom thema by user connected
 const osmTiles = "http://{s}.tile.osm.org/{z}/{x}/{y}.png";
 const osmAttr =
   '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+//Mapbox MapTiles - this constant could be pass in state for feature custom thema.
+const mapboxTiles = "https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZnJlZDc4OTYiLCJhIjoiY2p1YmJ2dnM5MDRkYTN6cW1nZHJoc3pudiJ9.UdKtx13HOR9-Uoej4C5cyw"
+const mapboxAttr = '&copy; <a href="https://www.mapbox.com/feedback/">Mapbox</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 
 export default class MyMap extends Component {
   constructor(props) {
@@ -25,11 +26,7 @@ export default class MyMap extends Component {
           center={this.state.center}
           zoom={this.state.zoomLevel}
         >
-          <MapboxLayer
-            accessToken={MAPBOX_ACCESS_TOKEN}
-             style="mapbox://styles/fred7896/cjubc0ed70c8z1fphm80anfgp" 
-             />
-          <TileLayer attribution={osmAttr} url={osmTiles} />
+          <TileLayer attribution={mapboxAttr} url={mapboxTiles} />
           {this.props.stations.map((station, i) => {
             return (
               <Marker position={station.fields.geo} key={i}>
