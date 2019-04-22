c30ffeada57ed4f35fd76a042a933c7801b1ebbf
Sabine Robart <sabine.robart@protonmail.com>
2019-04-22 08:41:06 +0200

    Add popup component with basic structure.

---
+++ moov-in-lille/src/component/MyPopup.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Popup } from "react-leaflet";
+
+export default class MyPopup extends React.Component {
+  render() {
+    const station = this.props.station;
+    const lowerCaseTitle = station.fields.nom.charAt(0).toUpperCase();
+    const captializeTitle = station.fields.nom.slice(1).toLowerCase();
+    const title = lowerCaseTitle + captializeTitle;
+    return (
+      <Popup>
+        <div className="row popup-container justify-content-center">
+          <div className="col-12 text-center">
+            <h5 className="m-0">{title}</h5>
+            <div className="separation mx-auto" />
+          </div>
+        </div>
+      </Popup>
+    );
+  }
+}
