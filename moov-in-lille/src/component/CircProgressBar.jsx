4f8013a1c288a87448c463dca480950ee6a41f07
Sabine Robart <sabine.robart@protonmail.com>
2019-04-22 09:50:55 +0200

    Add colors to the circular progress bar.

--- moov-in-lille/src/component/CircProgressBar.jsx
+++ moov-in-lille/src/component/CircProgressBar.jsx
@@ -8,6 +8,31 @@ class CircProgressBar extends React.Component {
     const max =
       this.props.stationSlider.fields.nbvelosdispo +
       this.props.stationSlider.fields.nbplacesdispo;
+    let percentage = (nbBikes * 100) / max;
+    const disable = nbBikes === 0 ? "#ccc" : "#b71332";
+    let rangeColor;
+
+    switch (true) {
+      case percentage === 0:
+        rangeColor = "#A5A5A5";
+        break;
+      case percentage >= 1 && percentage <= 25:
+        rangeColor = "#b71332";
+        break;
+      case percentage > 25 && percentage <= 50:
+        rangeColor = "#ff7300";
+        break;
+      case percentage > 50 && percentage <= 75:
+        rangeColor = "#F0F282";
+        break;
+      case percentage > 75 && percentage <= 100:
+        rangeColor = "#1ea500";
+        break;
+      default:
+        rangeColor = "#A5A5A5";
+        break;
+    }
+
     return (
       <div>
         <CircleSlider
@@ -17,9 +42,9 @@ class CircProgressBar extends React.Component {
           progressWidth="15"
           circleWidth="10"
           knobRadius="0"
-          // progressColor={rangeColor}
+          progressColor={rangeColor}
           showTooltip="true"
-          // tooltipColor={disable}
+          tooltipColor={disable}
         />
         <img
           src={require("../pictures/bike-icon.png")}
