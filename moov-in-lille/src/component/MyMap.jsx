663697959635237563627cac7a6e02eecf40ee7a
FredericMentel <frederic.mentel@gmail.com>
2019-04-23 09:10:17 +0200

    delete comment on line 4

--- moov-in-lille/src/component/MyMap.jsx
+++ moov-in-lille/src/component/MyMap.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import RangeSlider from "./RangeSlider";
 
-//Mapbox MapTiles - this constant could be pass in state for feature custom thema.
 const mapboxTiles = "https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZnJlZDc4OTYiLCJhIjoiY2p1YmJ2dnM5MDRkYTN6cW1nZHJoc3pudiJ9.UdKtx13HOR9-Uoej4C5cyw"
 const mapboxAttr = '&copy; <a href="https://www.mapbox.com/feedback/">Mapbox</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 
