dc045c784e3c887c92328596225905df1ddd2898
FredericMentel <frederic.mentel@gmail.com>
2019-04-24 15:34:47 +0200

    first try of integration of searchBar

--- moov-in-lille/src/component/SearchBar.jsx
+++ moov-in-lille/src/component/SearchBar.jsx
@@ -30,32 +30,33 @@ class SearchBar extends React.Component {
         <div className="search-box mb-2">
           <input
             type="text"
-            className="search-txt"
             placeholder="Rechercher"
             value={this.state.search}
             onChange={this.updateSearch.bind(this)}
           />
-          <a className="search-btn">
-            <i className="fas fa-search" />
-          </a>
+          <div class="search" />
         </div>
-        <ul className="res">
-          {filteredStations.length < 80 ? (
-            filteredStations.map((station, index) => {
-              return (
-                <li
-                  key={index}
-                  onClick={() => this.handleClick(station.fields.geo)}
-                  className="res"
-                >
-                  {station.fields.nom}
-                </li>
-              );
-            })
-          ) : (
-            <li className="res">Pas de résultat</li>
-          )}
-        </ul>
+        {this.state.search !== "" ? (
+          <ul className="res">
+            {filteredStations.length < 80 ? (
+              filteredStations.map((station, index) => {
+                return (
+                  <li
+                    key={index}
+                    onClick={() => this.handleClick(station.fields.geo)}
+                    className="res"
+                  >
+                    {station.fields.nom}
+                  </li>
+                );
+              })
+            ) : (
+              <li className="res">Pas de résultat</li>
+            )}
+          </ul>
+        ) : (
+          <p />
+        )}
       </div>
     );
   }
