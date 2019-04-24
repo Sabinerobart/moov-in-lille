9373df92d2121e7cb3e097c882b8be3e0fcd7002
Axelle Adamkiewicz <axelle.adamkiewicz@gmail.com>
2019-04-24 17:16:50 +0200

    Adding the icons and personnalisation of the navbar (so, colors and img). Adding Favoris and Itinéraire. I also resolved the problems of the scrollbar by readjusting the sizes of the navbar, the footer and the body-map. Not totally responsive yet for the icons in the navbar.

--- moov-in-lille/src/component/NavbarFull.jsx
+++ moov-in-lille/src/component/NavbarFull.jsx
@@ -1,52 +1,60 @@
-import React from 'react';
+import React from "react";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink, } from 'reactstrap';
-import '../App.css';
-import logo from '../img/logo.png';
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+import logo from "../img/logo.png";
+import itineraire from "../pictures/icone-itineraire.png";
+import favoris from "../pictures/icone-favoris.png";
 
 class NavbarFull extends React.Component {
-    constructor(props) {
-        super(props);
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-          isOpen: false,
-          isRightPanelOpen: false
-        };
-    }
-
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+      isRightPanelOpen: false
     };
+  }
 
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
 
-    render() {
-        return (
-            <React.Fragment>
-                <Navbar color="light" light expand="md">
-                    <NavbarBrand href="/"><img className="logo" src={logo} alt="logo"></img></NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="#">Itinéraire</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="#">Favoris</NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-            </React.Fragment>
-        );
-    }
-};
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">
+            <img className="logo" src={logo} alt="logo" />
+            <h1 className='accroche'> Trouvez votre V'Lille en 1 clic !</h1>
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem className="iconNav">
+                <NavLink href="#"><img src={itineraire} alt="Itinéraire" className="iconeiti"/><br/>
+                  Itinéraire
+                </NavLink>
+              </NavItem>
+              
+              <NavItem  className="iconNav">
+                <NavLink href="#"><img src={favoris} alt="Favoris" className="iconefav"/><br/>
+                Favoris</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </React.Fragment>
+    );
+  }
+}
 
 export default NavbarFull;
