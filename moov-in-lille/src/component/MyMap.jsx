03e1de746f16b2f013e5c3554088778197eecfac
Axelle Adamkiewicz <axelle.adamkiewicz@gmail.com>
2019-04-24 17:29:04 +0200

    keeping my work

--- moov-in-lille/src/component/MyMap.jsx
+++ moov-in-lille/src/component/MyMap.jsx
@@ -1,81 +1,53 @@
 import React, { Component } from "react";
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { Map, TileLayer } from "react-leaflet";
+import Markers from "./Markers";
+import Control from "react-leaflet-control";
+import "../App.scss";
 
-const osmTiles = "http://{s}.tile.osm.org/{z}/{x}/{y}.png";
-const osmAttr =
-  '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
-const mapCenter = [50.633333, 3.066667];
-
-const zoomLevel = 14;
+const mapboxTiles =
+  "https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZnJlZDc4OTYiLCJhIjoiY2p1YmJ2dnM5MDRkYTN6cW1nZHJoc3pudiJ9.UdKtx13HOR9-Uoej4C5cyw";
+const mapboxAttr =
+  '&copy; <a href="https://www.mapbox.com/feedback/">Mapbox</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
 export default class MyMap extends Component {
   constructor(props) {
     super(props);
-    this.state = { currentZoomLevel: zoomLevel };
+    this.state = {
+      zoomLevel: 16,
+      location: {
+        lat: 50.633333,
+        lng: 3.066667
+      }
+    };
   }
 
-  componentDidMount() {
-    const leafletMap = this.leafletMap.leafletElement;
-    leafletMap.on("zoomend", () => {
-      const updatedZoomLevel = leafletMap.getZoom();
-      this.handleZoomLevelChange(updatedZoomLevel);
+  handleGeoloc() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({
+        location: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        },
+        zoomLevel: 15
+      });
     });
-  }
-
-  handleZoomLevelChange(newZoomLevel) {
-    this.setState({ currentZoomLevel: newZoomLevel });
   }
 
   render() {
     return (
-      <div>
-        <Map
-          ref={m => {
-            this.leafletMap = m;
-          }}
-          center={mapCenter}
-          zoom={zoomLevel}
-        >
-          <TileLayer attribution={osmAttr} url={osmTiles} />
-          <Marker position={mapCenter}>
-            <Popup>
-              <div className="row popup-container">
-                <div className="col-6 text-center vertical-separation">
-                  <img
-                    src={require("../pictures/bike-icon.png")}
-                    alt="bike"
-                    width="33px"
-                    className="py-1"
-                  />
-                  <p className="text-center count-text">11</p>
-                </div>
-                <div className="col-6 text-center">
-                  <img
-                    src={require("../pictures/blue-parking.png")}
-                    alt="available spots"
-                    width="40px"
-                  />
-                  <p className="text-center count-text">3</p>
-                </div>
-                <div className="col-9 my-2">
-                  Rue Edouard Delesalle
-                  <br />
-                  59000 Lille
-                </div>
-                <div className="col-3 my-2 py-2">
-                  <i class="fas fa-heart" />
-                </div>
-                <a
-                  href="https://opendata.lillemetropole.fr/explore/dataset/vlille-realtime/information/?flg=fr"
-                  className="mx-3"
-                >
-                  Avis
-                </a>
-              </div>
-            </Popup>
-          </Marker>
-        </Map>
-      </div>
+      <Map center={this.state.location} zoom={this.state.zoomLevel}>
+        <TileLayer attribution={mapboxAttr} url={mapboxTiles} />
+        <Control position="topleft">
+          <button
+            onClick={() => this.handleGeoloc()}
+            className="geoloc-btn"
+            title="Show me where I am !"
+          >
+            <i className="fas fa-crosshairs" />
+          </button>
+        </Control>
+        <Markers stations = {this.props.stations}/>
+      </Map>
     );
   }
 }
