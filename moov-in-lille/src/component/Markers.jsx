c616aa68843453cf16f7342a69ec0000c6bff074
Sabine Robart <sabine.robart@protonmail.com>
2019-04-25 17:02:24 +0200

    Add function to center map when marker is clicked.

--- moov-in-lille/src/component/Markers.jsx
+++ moov-in-lille/src/component/Markers.jsx
@@ -1,116 +1,151 @@
-import React, { Component } from 'react';
-import {Marker} from 'react-leaflet';
-import L from 'leaflet';
-import MyPopup from './MyPopup';
-
+import React, { Component } from "react";
+import { Marker } from "react-leaflet";
+import L from "leaflet";
+import MyPopup from "./MyPopup";
 
 const redStripedMarker = new L.Icon({
-    iconUrl: require('../pictures/5.png'),
-    iconRetinaUrl: require('../pictures/5.png'),
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -35],
-    iconSize: [40, 40] 
-})
+  iconUrl: require("../pictures/5.png"),
+  iconRetinaUrl: require("../pictures/5.png"),
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -35],
+  iconSize: [40, 40]
+});
 const redMarker = new L.Icon({
-    iconUrl: require('../pictures/1-25.png'),
-    iconRetinaUrl: require('../pictures/1-25.png'),
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -35],
-    iconSize: [40, 40] 
-})
+  iconUrl: require("../pictures/1-25.png"),
+  iconRetinaUrl: require("../pictures/1-25.png"),
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -35],
+  iconSize: [40, 40]
+});
 const orangeMarker = new L.Icon({
-    iconUrl: require('../pictures/25-50.png'),
-    iconRetinaUrl: require('../pictures/25-50.png'),
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -35],
-    iconSize: [40, 40] 
-})
+  iconUrl: require("../pictures/25-50.png"),
+  iconRetinaUrl: require("../pictures/25-50.png"),
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -35],
+  iconSize: [40, 40]
+});
 const yellowMarker = new L.Icon({
-    iconUrl: require('../pictures/50-75.png'),
-    iconRetinaUrl: require('../pictures/50-75.png'),
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -35],
-    iconSize: [40, 40] 
-})
+  iconUrl: require("../pictures/50-75.png"),
+  iconRetinaUrl: require("../pictures/50-75.png"),
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -35],
+  iconSize: [40, 40]
+});
 const greenMarker = new L.Icon({
-    iconUrl: require('../pictures/75-100.png'),
-    iconRetinaUrl: require('../pictures/75-100.png'),
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -35],
-    iconSize: [40, 40] 
-})
+  iconUrl: require("../pictures/75-100.png"),
+  iconRetinaUrl: require("../pictures/75-100.png"),
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -35],
+  iconSize: [40, 40]
+});
 const greyMarker = new L.Icon({
-    iconUrl: require('../pictures/indispo.png'),
-    iconRetinaUrl: require('../pictures/indispo.png'),
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -35],
-    iconSize: [40, 40] 
-})
+  iconUrl: require("../pictures/indispo.png"),
+  iconRetinaUrl: require("../pictures/indispo.png"),
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -35],
+  iconSize: [40, 40]
+});
 
 const fullBikeMarker = new L.Icon({
-    iconUrl: require('../pictures/fullbike.png'),
-    iconRetinaUrl: require('../pictures/fullbike.png'),
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -35],
-    iconSize: [40, 40] 
-})
+  iconUrl: require("../pictures/fullbike.png"),
+  iconRetinaUrl: require("../pictures/fullbike.png"),
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -35],
+  iconSize: [40, 40]
+});
 
 class Markers extends Component {
-    render() {
-        return(
-            this.props.stations.map((station, key) => {
-                const freeBike = station.fields.nbvelosdispo;
-                const freeSpot = station.fields.nbplacesdispo;
-                const totalSpot = freeBike + freeSpot;
-                const percentageFreeBike = (100 * freeBike) / totalSpot;
-                switch (true) {
-                    case (percentageFreeBike === 0):
-                    return(
-                        <Marker key={key} position={station.fields.geo} icon={redStripedMarker}>
-                            <MyPopup station = {station}/>
-                        </Marker>
-                    )
-                    case (percentageFreeBike >= 1 && percentageFreeBike <= 25):
-                        return (
-                        <Marker key={key} position={station.fields.geo} icon={redMarker}>
-                            <MyPopup station = {station}/>
-                        </Marker>
-                        )
-                    case (percentageFreeBike > 25 && percentageFreeBike <= 50):
-                        return(
-                        <Marker key={key} position={station.fields.geo} icon={orangeMarker}>
-                            <MyPopup station = {station}/>
-                        </Marker>
-                        )
-                    case (percentageFreeBike > 50 && percentageFreeBike <= 75):
-                        return(
-                        <Marker key={key} position={station.fields.geo} icon={yellowMarker}>
-                            <MyPopup station = {station}/>
-                        </Marker>
-                        )
-                    case (percentageFreeBike > 75 && percentageFreeBike <= 100):
-                        return(
-                        <Marker key={key} position={station.fields.geo} icon={greenMarker}>
-                            <MyPopup station = {station}/>
-                        </Marker>
-                        )
-                    case (percentageFreeBike === 100):
-                    return(
-                        <Marker key={key} position={station.fields.geo} icon={fullBikeMarker}>
-                            <MyPopup station = {station}/>
-                        </Marker>
-                    )
-                    default: 
-                    return(
-                        <Marker key={key} position={station.fields.geo} icon={greyMarker}>
-                            <MyPopup station = {station}/>
-                        </Marker>
-                        )
-                        
-                }
-            })
-        )
-    };
-};
+  getCenter(coord) {
+    this.props.getCenter(coord);
+  }
+
+  render() {
+    return this.props.stations.map((station, key) => {
+      const freeBike = station.fields.nbvelosdispo;
+      const freeSpot = station.fields.nbplacesdispo;
+      const totalSpot = freeBike + freeSpot;
+      const percentageFreeBike = (100 * freeBike) / totalSpot;
+      switch (true) {
+        case percentageFreeBike === 0:
+          return (
+            <Marker
+              key={key}
+              position={station.fields.geo}
+              icon={redStripedMarker}
+              onClick={() => this.getCenter(station.fields.geo)}
+            >
+              <MyPopup station={station} />
+            </Marker>
+          );
+        case percentageFreeBike >= 1 && percentageFreeBike <= 25:
+          return (
+            <Marker
+              key={key}
+              position={station.fields.geo}
+              icon={redMarker}
+              onClick={() => this.getCenter(station.fields.geo)}
+            >
+              <MyPopup station={station} />
+            </Marker>
+          );
+        case percentageFreeBike > 25 && percentageFreeBike <= 50:
+          return (
+            <Marker
+              key={key}
+              position={station.fields.geo}
+              icon={orangeMarker}
+              onClick={() => this.getCenter(station.fields.geo)}
+            >
+              <MyPopup station={station} />
+            </Marker>
+          );
+        case percentageFreeBike > 50 && percentageFreeBike <= 75:
+          return (
+            <Marker
+              key={key}
+              position={station.fields.geo}
+              icon={yellowMarker}
+              onClick={() => this.getCenter(station.fields.geo)}
+            >
+              <MyPopup station={station} />
+            </Marker>
+          );
+        case percentageFreeBike > 75 && percentageFreeBike <= 100:
+          return (
+            <Marker
+              key={key}
+              position={station.fields.geo}
+              icon={greenMarker}
+              onClick={() => this.getCenter(station.fields.geo)}
+            >
+              <MyPopup station={station} />
+            </Marker>
+          );
+        case percentageFreeBike === 100:
+          return (
+            <Marker
+              key={key}
+              position={station.fields.geo}
+              icon={fullBikeMarker}
+              onClick={() => this.getCenter(station.fields.geo)}
+            >
+              <MyPopup station={station} />
+            </Marker>
+          );
+        default:
+          return (
+            <Marker
+              key={key}
+              position={station.fields.geo}
+              icon={greyMarker}
+              onClick={() => this.getCenter(station.fields.geo)}
+            >
+              <MyPopup station={station} />
+            </Marker>
+          );
+      }
+    });
+  }
+}
 
 export default Markers;
