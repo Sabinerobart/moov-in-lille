6a0ab7fc1353b3114c6967d486d5eaedfc665449
Alexy Samyn <alexy59430@gmail.com>
2019-04-26 13:31:12 +0200

    add form to connect

--- moov-in-lille/src/component/FavoritesPanel.jsx
+++ moov-in-lille/src/component/FavoritesPanel.jsx
@@ -2,10 +2,42 @@ import React from 'react';
 import '../App.scss';
 
 class FavoritesPanel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          isOpen: false,
+          identity: ''
+        };
+        this.onChange = this.onChange.bind(this);
+        this.submitForm = this.submitForm.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    submitForm(e) {
+        e.preventDefault();
+        console.log(this.state.identity)
+    };
     render() {
         return(
             <div>
-                <h1>Favoris</h1>
+                <form onSubmit={this.submitForm}>
+                    <div className="form-data">
+                        <label htmlFor="lastname">Identifiant:</label>
+                        <input
+                            type="text"
+                            id="identity"
+                            name="identity"
+                            onChange={this.onChange}
+                            value={this.state.identity}
+                        />
+                        <button type="submit" value="OK">OK</button>
+                    </div>
+                </form>
             </div>
         )
     }
