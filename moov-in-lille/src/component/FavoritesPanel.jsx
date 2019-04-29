42c20f3f6b88117045cfe238f757a6641e94d972
Axelle Adamkiewicz <axelle.adamkiewicz@gmail.com>
2019-04-29 14:01:00 +0200

    Adding the overflow on the Favorites. You can scroll now.

--- moov-in-lille/src/component/FavoritesPanel.jsx
+++ moov-in-lille/src/component/FavoritesPanel.jsx
@@ -20,31 +20,35 @@ class FavoritesPanel extends React.Component {
       });
   }
   render() {
-    return this.state.user.map(usr => {
-      return usr.favoris.map((fav, i) => {
-        return (
-          <div key={i} className="favorites">
-            <h4 className="titlefavorite">{fav.emplacement}</h4>
-            {this.props.stations
-              .filter(station => {
-                return station.fields.nom === fav.nom;
-              })
-              .map((station, i) => {
-                return (
-                  <React.Fragment key={i}>
-                    <p>{station.fields.nom}</p>
-                    <p>{station.fields.adresse}</p>
-                    <p>
-                      {station.fields.nbvelosdispo} vélo,{" "}
-                      {station.fields.nbplacesdispo} places
-                    </p>
-                  </React.Fragment>
-                );
-              })}
-          </div>
-        );
-      });
-    });
+    return (
+      <div className="allfavorite">
+        {this.state.user.map(usr => {
+          return usr.favoris.map((fav, i) => {
+            return (
+              <div key={i} className="favorites">
+                <h4 className="titlefavorite">{fav.emplacement}</h4>
+                {this.props.stations
+                  .filter(station => {
+                    return station.fields.nom === fav.nom;
+                  })
+                  .map((station, i) => {
+                    return (
+                      <React.Fragment key={i}>
+                        <p>{station.fields.nom}</p>
+                        <p>{station.fields.adresse}</p>
+                        <p>
+                          {station.fields.nbvelosdispo} vélo,{" "}
+                          {station.fields.nbplacesdispo} places
+                        </p>
+                      </React.Fragment>
+                    );
+                  })}
+              </div>
+            );
+          });
+        })}
+      </div>
+    );
   }
 }
 
