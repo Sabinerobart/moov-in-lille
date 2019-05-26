cee66d2a91b66a955a78cb7c3588769ea97cfe79
Sabine Robart <sabine.robart@protonmail.com>
2019-05-26 11:16:47 +0200

    [Refa] move variables.

--- moov-in-lille/src/component/MyMap.jsx
+++ moov-in-lille/src/component/MyMap.jsx
@@ -5,9 +5,8 @@ import Markers from "./Markers";
 import Control from "react-leaflet-control";
 import "../App.scss";
 import Routing from "./Routing";
+import mapboxTiles from "../conf";
 
-const mapboxTiles =
-  "https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZnJlZDc4OTYiLCJhIjoiY2p1YmJ2dnM5MDRkYTN6cW1nZHJoc3pudiJ9.UdKtx13HOR9-Uoej4C5cyw";
 const mapboxAttr =
   '&copy; <a href="https://www.mapbox.com/feedback/">Mapbox</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
